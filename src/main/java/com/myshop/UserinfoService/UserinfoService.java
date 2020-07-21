240fb16cb618e4755ce0742fd4d02ebab961917a
mwhe-lrving <2598121166@qq.com>
2020-07-21 22:11:50 +0800

    Service接口实现类

---
+++ src/main/java/com/myshop/UserinfoService/UserinfoService.java
@@ -0,0 +1,9 @@
+package com.myshop.UserinfoService;
+
+import com.myshop.pojo.Userinfo;
+
+public interface UserinfoService {
+    public Userinfo login(Userinfo user);
+
+    public Userinfo register(Userinfo user);
+}
